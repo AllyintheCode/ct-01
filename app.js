32da4b1048f5a508692b284e98317696d32200d7
AllyintheCode <allyasadzade@gmail.com>
2025-03-17 01:08:01 +0400

    home task complated

---
+++ app.js
@@ -0,0 +1,67 @@
+// function IlkHerifDeyis(soz, ilkherif) {
+//   return soz.replace(soz[0], ilkherif)
+// }
+// console.log(IlkHerifDeyis("qara", "y"));
+
+// function basHerfBoyut(text) {
+//   return text[0].toUpperCase() + text.slice(1)
+// }
+// console.log(basHerfBoyut("salam"));
+
+
+// function hamisiBoyuk(metn) {
+//   return metn.toUpperCase()
+// }
+// console.log(hamisiBoyuk("salam"));
+
+
+// function hamisiKicik(soz) {
+//   return soz.toLowerCase()
+// }
+// console.log(hamisiKicik("SALAM"));
+
+
+// function herfTapan(soz, herf) {
+//   return soz.includes("a")
+// }
+// console.log(herfTapan("salam", "a"));
+
+
+// function boyukYox(soz) {
+//   return soz === soz.toUpperCase()
+// }
+
+// console.log(boyukYox("SALAM"));
+
+
+// function kicikYox(soz) {
+//   return soz === soz.toLowerCase()
+// }
+
+// console.log(kicikYox("salam"));
+
+// function boyTapan(soz) {
+//   for (let i = 0; i < soz.length; i++) {
+//     let char = soz[i]
+//     if (char >= 'A' && char <= 'Z') {
+//       return char
+//     } else {
+//       console.log("tapilmadi");
+
+//     }
+//   }
+// }
+
+// console.log(boyTapan("Salam dunnya"));
+
+
+// function reqemTapan(reqem) {
+//   for (let i = 0; i < reqem.length; i++) {
+//     let char = reqem[i]
+//     if (char >= '0' && char <= "9") {
+//       return char
+//     }
+//   }
+// }
+
+// console.log(reqemTapan("salam menim 6 yasim var"));
